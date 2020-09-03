f86c97cac534f1bce16144e3b3e1eb16997a6b27
orderthe13th <68018307+orderthe13th@users.noreply.github.com>
2020-09-04 00:45:54 +0530

    check

---
+++ Assets/Scripts/Forward Version/ConstantShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private float timeBetweenShoots;
+    [SerializeField] private int laserPerShoot;
+
+    private bool isShooting;
+
+    private void Start()
+    {
+
+    }
+
+}
